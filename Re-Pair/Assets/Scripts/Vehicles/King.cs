2f39a221fa340e829cf0777ced35225c2ee597f3
wholagan <ruscav@gmail.com>
2020-02-01 17:21:40 -0800

    Here you go

---
+++ Re-Pair/Assets/Scripts/Vehicles/King.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// NOTE: king will have special implementation
+public class King : Vehicle
+{
+	// movement
+    public override List<Vector2Int> MoveLocations(Vector2Int gridPoint) {
+        List<Vector2Int> locations = new List<Vector2Int>();
+        List<Vector2Int> directions = new List<Vector2Int>(BishopDirections);
+        directions.AddRange(RookDirections);
+
+        foreach (Vector2Int dir in directions)
+        {
+            Vector2Int nextGridPoint = new Vector2Int(gridPoint.x + dir.x, gridPoint.y + dir.y);
+            locations.Add(nextGridPoint);
+        }
+
+        return locations;
+    }
+	
+	// projectiles
+	public override List<Vector2Int> ShootLocations(Vector2Int gridPoint) {
+        List<Vector2Int> locations = new List<Vector2Int>();
+        List<Vector2Int> directions = new List<Vector2Int>(BishopDirections);
+        directions.AddRange(RookDirections);
+
+        foreach (Vector2Int dir in directions)
+        {
+            Vector2Int nextGridPoint = new Vector2Int(gridPoint.x + dir.x, gridPoint.y + dir.y);
+            locations.Add(nextGridPoint);
+        }
+
+        return locations;
+    }
+    }
+}
---
+++ Re-Pair/Assets/Scripts/Vehicles/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum VehicleType {King, Mortar, Artillery, TankDestroyer, Tank, Drone};
+
+public abstract class Vehicle : MonoBehaviour {
+    public VehicleType type;
+	
+	protected Vector2Int[] RookDirections = {new Vector2Int(0,1), new Vector2Int(1, 0),
+        new Vector2Int(0, -1), new Vector2Int(-1, 0)};
+    protected Vector2Int[] BishopDirections = {new Vector2Int(1,1), new Vector2Int(1, -1),
+        new Vector2Int(-1, -1), new Vector2Int(-1, 1)};
+	
+	// movement
+    public abstract List<Vector2Int> MoveLocations(Vector2Int gridPoint);
+	
+	// projectiles
+	public abstract List<Vector2Int> ShootLocations(Vector2Int gridPoint);
+}
